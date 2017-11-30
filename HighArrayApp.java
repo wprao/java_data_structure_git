a02e928ee579a21d38207955d7645a673c727fbe
wprao <wprao_hust@126.com>
2017-11-30 09:34:01 +0800

    add half-search

--- HighArrayApp.java
+++ HighArrayApp.java
@@ -22,6 +22,56 @@ class HighArray
 	else return true;
     }
 
+    public int find_half_search(long searchKey)
+    {
+	int lower_bound,higher_bound;
+	lower_bound = 0;
+	higher_bound = nElems - 1;
+
+	int current_key;
+
+	while(true)
+	    {
+		current_key = (lower_bound + higher_bound) / 2;
+		if(mArray[current_key] == searchKey)
+		    {
+			//			System.out.println("find it in " + searchKey);
+			return current_key;
+		    }
+		else if(lower_bound > higher_bound)
+		    {
+			return -1;       //can't find it;
+			//		System.out.println("can't find" + searchKey);
+		    }
+		else
+		    {
+			if(mArray[current_key] < searchKey)
+			    lower_bound = current_key + 1;//search is beyond current;
+			else if(mArray[current_key] > searchKey)
+			    higher_bound = current_key - 1;//search is below current;
+		    }
+	    }
+	
+	
+    }
+
+
+    public void insert_half_search(long value)   //insert in order
+    {
+	int j;
+	for(j=0;j<nElems;j++)
+	    {
+		if(mArray[j]> value)
+		    break;
+	    }
+	for(int k=nElems;k>j;k--)
+	    {
+		mArray[k] = mArray[k-1];
+	    }
+	mArray[j] = value;
+	nElems++;
+    }
+    
     public void insert(long value)
     {
 	mArray[nElems] = value;
@@ -70,17 +120,18 @@ class HighArrayApp
 	arr = new HighArray(max_size);
 
 	for(int j=0;j<125;j++)
-	    arr.insert((long)(Math.random()*100.0));
+	    arr.insert_half_search((long)(Math.random()*100.0));
 
 	
 	arr.display();
 
-	if(arr.find(77))
+	if(arr.find_half_search(1) >= 0)
 	    {
-		System.out.println("found 77!");
-		arr.delete(77);
+		System.out.println("found 1! in " + arr.find_half_search(1));
+		//	arr.delete(1);
 	    
 	    }
-	arr.display();
+	else  System.out.println("can't find it");
+	//	arr.display();
     }
 }
